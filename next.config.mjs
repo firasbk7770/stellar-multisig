65f408d32398149056616802935d06a61c0f105c
FullStackDev001 <162185577+firasbk7770@users.noreply.github.com>
2024-07-03 20:22:03 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,5 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix,
-    basePath,
-    images: {
-        unoptimized: true,
-    },
 };
 
 export default nextConfig;
